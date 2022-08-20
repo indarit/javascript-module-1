1e07c924088eb9db927e959deeea2b1540e8fef9
indarit <109621400+indarit@users.noreply.github.com>
2022-08-20 13:15:44 +0200

    exercise2.js del J-functions completado

--- week-1/Homework/J-functions/exercise.js
+++ week-1/Homework/J-functions/exercise.js
@@ -1,7 +1,9 @@
 function halve(number) {
   return number/2
 }
-let result = halve(12)
+let result = halve(12);
 console.log(result);
-result= halve(18)
-console.log(result)
+result= halve(18);
+console.log(result);
+result= halve(20);
+console.log(result);
