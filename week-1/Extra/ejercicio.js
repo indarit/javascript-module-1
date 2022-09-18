e26b60e5767dc170d87b8b626eb12d2856d7b763
indarit <109621400+indarit@users.noreply.github.com>
2022-09-18 15:54:02 +0200

    ejecicios listo

---
+++ week-1/Extra/ejercicio.js
@@ -0,0 +1,12 @@
+let isHappy = "happy";
+
+if (isHappy) {
+  console.log("Good job, you're doing great!" if you pass in "happy");
+} else {
+  console.log("Every cloud has a silver lining" if you pass in "sad");
+}
+  else if (isHappy typeof Number) {
+    return "Beep beep boop" if you pass in a number";
+}
+else if (isHappy typeof Number;) {
+    return "Beep beep boop" if you pass in a number";
