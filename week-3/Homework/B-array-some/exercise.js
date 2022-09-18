e6353e3ebfc1ab2e8df10ec3145b398969b56b04
indarit <109621400+indarit@users.noreply.github.com>
2022-09-18 16:01:17 +0200

    excercise B guardado

--- week-3/Homework/B-array-some/exercise.js
+++ week-3/Homework/B-array-some/exercise.js
@@ -11,7 +11,14 @@ var pairsByIndex = [[0, 3], [1, 2], [2, 1], null, [3, 0]];
 // If there is a null value in the array exit the program with the error code
 // https://nodejs.org/api/process.html#process_process_exit_code
 // process.exit(1);
+const connull = pairsByIndex.some(element =>{
+  return element === null;
+});
 
+if(connull){
+  console.log("Hay un null en el array.");
+  process.exit(1);
+}
 var students = ["Islam", "Lesley", "Harun", "Rukmini"];
 var mentors = ["Daniel", "Irina", "Mozafar", "Luke"];
 
