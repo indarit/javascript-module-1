88ce708c87092da06c6926da3e3100e02f4c868a
indarit <109621400+indarit@users.noreply.github.com>
2022-09-18 15:55:37 +0200

    ejecisio F guandado

--- week-1/Homework/F-strings-methods/exercise.js
+++ week-1/Homework/F-strings-methods/exercise.js
@@ -1,4 +1,6 @@
 // Start by creating a variable `message`
+
+
 const nombre = "Andrea"
 const longitudNombre= nombre.length
 console.log("My name is " + nombre + " and my name is " + longitudNombre + " characters long");
